a2788011e86efe31bd60dac28bcac35658735cca
vanssign <thindvansh1999@gmail.com>
2021-03-06 13:46:42 +0530

    change

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Something Something</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
